7e592cf129a485fd30e460ecb17c5b6eb2c89f72
Miltonvaz <233427@ids.upchiapas.edu.mx>
2024-11-04 14:48:54 -0600

    primer commit

---
+++ src/app/components/detalles/detalles.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ApiPokemonService } from '../../service/api-pokemon.service';
+import { Pokemon } from '../../models/pokemon';
+import { TitleCasePipe } from '@angular/common';
+import { HeaderComponent } from '../header/header.component';
+import { FooterComponent } from '../footer/footer.component';
+import { UpperCasePipe } from '@angular/common';
+
+@Component({
+  selector: 'app-detalles',
+  imports: [CommonModule, TitleCasePipe, HeaderComponent, FooterComponent],
+  standalone: true,
+  templateUrl: './detalles.component.html',
+  styleUrls: ['./detalles.component.css']
+})
+export class DetallesComponent implements OnInit {
+  pokemon?: Pokemon;
+  pokemonImages: string[] = [];
+
+  constructor(private route: ActivatedRoute, private apiService: ApiPokemonService) {}
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      const url = params['url'];
+      if (url) {
+        this.apiService.getPokemonDetails(url).subscribe({
+          next: (data: Pokemon) => {
+            this.pokemon = data;
+            this.loadPokemonImages();
+          },
+          error: (err: any) => {
+            console.error('Error retrieving Pokémon details:', err);
+          }
+        });
+      }
+    });
+  }
+
+  loadPokemonImages(): void {
+    if (this.pokemon?.sprites) {
+      this.pokemonImages = [
+        this.pokemon.sprites.front_default,
+        this.pokemon.sprites.back_default,
+        this.pokemon.sprites.front_shiny,
+        this.pokemon.sprites.back_shiny,
+      ].filter((url): url is string => url !== undefined); 
+    }
+  
+  }
+}
